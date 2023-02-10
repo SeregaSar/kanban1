461c76a9724dcf82351996680f0e2f9152dc5d41
Sergey Saranyuk <saranyk82@gmail.com>
2023-02-10 11:37:02 -0500

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require("./routes");
 const bodyParser = require("./bodyParces");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 
 routes(app);
